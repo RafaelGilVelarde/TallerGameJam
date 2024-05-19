aaf4d63a8b995851ac764bc10c1408d57c36dcdf
RafaelGilVelarde <20192303@aloe.ulima.edu.pe>
2024-05-19 18:29:32 -0500

    cosas

--- MainMenuAssets/OpcionsMenu.cs
+++ MainMenuAssets/OpcionsMenu.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class OpcionsMenu : MonoBehaviour
 {
-    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private AudioMixer MainMixer, SFXMixer;
+    [SerializeField] float MainVolume, SFXVolume;
+    [SerializeField] Slider MainSlider, SFXSlider;
 
     public void FullScreen(bool fullcreen)
     {
         Screen.fullScreen = fullcreen;
     }
 
-    public void ChangeVolume(float volume)
+    public void ChangeVolume()
     {
-        audioMixer.SetFloat("Volume", volume);
+        MainVolume=MainSlider.value;
+        SFXVolume=SFXSlider.value;
+        MainMixer.SetFloat("Volume", Mathf.Log(MainVolume) * 20);
+        SFXMixer.SetFloat("Volume", Mathf.Log(MainVolume)*20);
     }
 
 }
